aac723fd13ef3e065ab17a610cd7eb154818c0f5
Sam Bahrami <sam@aiml.team>
2021-09-07 23:24:38 +0930

    re

--- object_detector/lightning.py
+++ object_detector/lightning.py
@@ -3,24 +3,25 @@ from torch import nn
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
-from torchvision.datasets import VOCDetection
+from torchvision.datasets import MNIST
 from torchvision import transforms
 import pytorch_lightning as pl
 
 
-class LitAutoEncoder(pl.LightningModule):
+class ImageClassifier(pl.LightningModule):
     def __init__(self):
         super().__init__()
-        self.encoder = nn.Sequential(
-            nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3)
-        )
-        self.decoder = nn.Sequential(
-            nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28)
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 6, 5),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(6, 16, 5),
+            nn.Linear(16 * 5 * 5, 120),
+            nn.Linear(120, 64),
+            nn.Linear(64, 20),
         )
 
     def forward(self, x):
-        embedding = self.encoder(x)
-        return embedding
+        return self.model(x)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
@@ -44,15 +45,14 @@ class LitAutoEncoder(pl.LightningModule):
         self.log("val_loss", loss)
 
 
-# data
-dataset = MNIST("", train=True, download=True, transform=transforms.ToTensor())
+dataset = MNIST("data", train=True, download=True, transform=transforms.ToTensor())
 mnist_train, mnist_val = random_split(dataset, [55000, 5000])
 
 train_loader = DataLoader(mnist_train, batch_size=32)
 val_loader = DataLoader(mnist_val, batch_size=32)
 
 # model
-model = LitAutoEncoder()
+model = ImageClassifier()
 
 # training
 trainer = pl.Trainer(gpus=1, num_nodes=1, precision=16, limit_train_batches=0.5)
