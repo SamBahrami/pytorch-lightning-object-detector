7884ff61ac55e3fb4d3a1241630b15fab1e5c0cd
Sam Bahrami <sam@aiml.team>
2021-09-08 16:27:28 +0930

    MNIST Image Classifier that works

--- object_detector/lightning.py
+++ object_detector/lightning.py
@@ -3,24 +3,33 @@ from torch import nn
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
-from torchvision.datasets import VOCDetection
+from torchvision.datasets import MNIST
 from torchvision import transforms
+from torchmetrics.functional import accuracy
 import pytorch_lightning as pl
 
 
-class LitAutoEncoder(pl.LightningModule):
+class ImageClassifier(pl.LightningModule):
     def __init__(self):
         super().__init__()
-        self.encoder = nn.Sequential(
-            nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3)
-        )
-        self.decoder = nn.Sequential(
-            nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28)
-        )
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        # we use the maxpool multiple times, but define it once
+        self.pool = nn.MaxPool2d(2,2)
+        # in_channels = 6 because self.conv1 output 6 channel
+        self.conv2 = nn.Conv2d(6,16,5) 
+        # 5*5 comes from the dimension of the last convnet layer
+        self.fc1 = nn.Linear(16*4*4, 120) 
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        embedding = self.encoder(x)
-        return embedding
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16*4*4)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)  # no activation on final layer 
+        return x
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
@@ -28,32 +37,32 @@ class LitAutoEncoder(pl.LightningModule):
 
     def training_step(self, train_batch, batch_idx):
         x, y = train_batch
-        x = x.view(x.size(0), -1)
-        z = self.encoder(x)
-        x_hat = self.decoder(z)
-        loss = F.mse_loss(x_hat, x)
-        self.log("train_loss", loss)
+        z = self.forward(x)
+        loss = F.cross_entropy(z, y)
+        acc = accuracy(z, y)
+        self.log("train/loss", loss)
+        self.log("train/acc", acc)
         return loss
 
     def validation_step(self, val_batch, batch_idx):
         x, y = val_batch
-        x = x.view(x.size(0), -1)
-        z = self.encoder(x)
-        x_hat = self.decoder(z)
-        loss = F.mse_loss(x_hat, x)
-        self.log("val_loss", loss)
+        z = self.forward(x)
+        loss = F.cross_entropy(z, y)
+        acc = accuracy(z, y)
+        self.log("val/loss", loss)
+        self.log("val/acc", acc)
 
 
 # data
-dataset = MNIST("", train=True, download=True, transform=transforms.ToTensor())
+dataset = MNIST("data", train=True, download=True, transform=transforms.ToTensor())
 mnist_train, mnist_val = random_split(dataset, [55000, 5000])
 
-train_loader = DataLoader(mnist_train, batch_size=32)
-val_loader = DataLoader(mnist_val, batch_size=32)
+train_loader = DataLoader(mnist_train, batch_size=256, num_workers=6)
+val_loader = DataLoader(mnist_val, batch_size=256, num_workers=6)
 
 # model
-model = LitAutoEncoder()
+model = ImageClassifier()
 
 # training
-trainer = pl.Trainer(gpus=1, num_nodes=1, precision=16, limit_train_batches=0.5)
+trainer = pl.Trainer(gpus=1, max_epochs=20)
 trainer.fit(model, train_loader, val_loader)
