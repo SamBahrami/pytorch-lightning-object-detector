81dba4ece21fd985578de50a7f2c7f41df2c1dd1
Sam Bahrami <sam@aiml.team>
2021-09-08 23:31:32 +0930

    windows compatibility multiprocessing https://stackoverflow.com/questions/18204782/runtimeerror-on-windows-trying-python-multiprocessing

--- object_detector/lightning.py
+++ object_detector/lightning.py
@@ -14,21 +14,21 @@ class ImageClassifier(pl.LightningModule):
         super().__init__()
         self.conv1 = nn.Conv2d(1, 6, 5)
         # we use the maxpool multiple times, but define it once
-        self.pool = nn.MaxPool2d(2,2)
+        self.pool = nn.MaxPool2d(2, 2)
         # in_channels = 6 because self.conv1 output 6 channel
-        self.conv2 = nn.Conv2d(6,16,5) 
+        self.conv2 = nn.Conv2d(6, 16, 5)
         # 5*5 comes from the dimension of the last convnet layer
-        self.fc1 = nn.Linear(16*4*4, 120) 
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16*4*4)
+        x = x.view(-1, 16 * 4 * 4)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)  # no activation on final layer 
+        x = self.fc3(x)  # no activation on final layer
         return x
 
     def configure_optimizers(self):
@@ -53,16 +53,21 @@ class ImageClassifier(pl.LightningModule):
         self.log("val/acc", acc)
 
 
-# data
-dataset = MNIST("data", train=True, download=True, transform=transforms.ToTensor())
-mnist_train, mnist_val = random_split(dataset, [55000, 5000])
+def main():
+    # data
+    dataset = MNIST("data", train=True, download=True, transform=transforms.ToTensor())
+    mnist_train, mnist_val = random_split(dataset, [55000, 5000])
 
-train_loader = DataLoader(mnist_train, batch_size=256, num_workers=6)
-val_loader = DataLoader(mnist_val, batch_size=256, num_workers=6)
+    train_loader = DataLoader(mnist_train, batch_size=256, num_workers=6)
+    val_loader = DataLoader(mnist_val, batch_size=256, num_workers=6)
 
-# model
-model = ImageClassifier()
+    # model
+    model = ImageClassifier()
 
-# training
-trainer = pl.Trainer(gpus=1, max_epochs=20)
-trainer.fit(model, train_loader, val_loader)
+    # training
+    trainer = pl.Trainer(gpus=1, max_epochs=20)
+    trainer.fit(model, train_loader, val_loader)
+
+
+if __name__ == "__main__":
+    main()
